85575438b9cba18d6540629b871552bbc2634ab3
Gui Barreto <barretogui90@gmail.com>
2021-07-19 20:33:45 -0300

    adicionando arquivo database.js ja com informações

---
+++ database.js
@@ -0,0 +1,45 @@
+const albuns = [{
+    id: 1,
+    artista: "Black Sabbath",
+    album: "Never Say Die",
+    ano: 1978,
+    estilo: "Heavy Metal"
+},
+{
+    id: 2,
+    artista: "Ozzy Osbourne",
+    album: "Blizzard Of Ozz",
+    ano: 1980,
+    estilo: "Heavy Metal"
+},
+{
+    id: 3,
+    artista: "Slayer",
+    album: "Hell Awaits",
+    ano: 1985,
+    estilo: "Thrash Metal",
+},
+{
+    id: 4,
+    artista: "Metallica",
+    album: "Ride the Lightning",
+    ano: 1984,
+    estilo: "Thrash Metal"
+},
+{
+    id: 5, 
+    artista: "Venom",
+    album:"Welcome to Hell",
+    ano: 1981,
+    estilo: "Black Metal"
+},
+{
+    id: 6,
+    artista: "Sarcófago",
+    album: "The Laws of Scourge",
+    ano: 1991,
+    estilo:"Death Metal"
+},
+]
+
+module.exports = albuns
